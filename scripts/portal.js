304878e410a831b326658f1ac340b23662fcc05f
definitely-a-human <mike.makes.robots.24123@gmail.com>
2023-01-09 19:13:41 -0700

    finished assignment portal

---
+++ scripts/portal.js
@@ -0,0 +1,5 @@
+const copyright = document.querySelector('#copyright');
+
+copyright.textContent = `© ${new Date().getFullYear()}`;
+
+let lastModif = document.getElementById("last-update").innerText = document.lastModified;
